512a4bf0185e7501d6f368dd1d66cce2c89ff7cf
Jenny <jiarmy1125@gmail.com>
2019-06-24 10:22:48 +0800

    class02_homework

--- class02_homework2.py
+++ class02_homework2.py
@@ -15,3 +15,19 @@ for num_2 in range(n): #印出結果
 
     else:
         print("Fib(",num_2,") = Fib(",num_2-2,") + Fib(",num_2-1,") = ",fibonacci(num_2))
+
+
+
+#使用for迴圈
+# n = int(input("請輸入第n項：")) 
+
+# x=[]
+# for num in range(n):    
+#     if (num==0 or num==1):  # 0,1
+#         x.append(num) #x[0,1]
+#         print("Fib(",num,") = ",x[num])
+
+#     else:
+#         a=[(x[num-1]+x[num-2])]     #3: 3-1,3-2
+#         x += a  #x[0,1,1,........]
+#         print("Fib(",num,") = Fib(",(num-2),") + Fib(",(num-1),") = ",x[num])
